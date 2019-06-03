2da0a60de2eaf30128e2e282db34a9b0809f3a1c
Joy Lin <vvlnote@hotmail.com>
2019-06-03 04:43:29 +0000

    Done.

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,4 +1,22 @@
 class Classification < ActiveRecord::Base
   has_many :boat_classifications
   has_many :boats, through: :boat_classifications
+
+  def self.my_all
+    self.all
+  end
+
+  def self.longest
+    longest = 0
+    boat = nil
+    self.all.each do |c|
+      c.boats.each do |b|
+        if b.length > longest
+          longest = b.length
+          boat = b
+        end
+      end
+    end
+    boat.classifications
+  end
 end
