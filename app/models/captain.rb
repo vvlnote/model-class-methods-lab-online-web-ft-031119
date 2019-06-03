d5c4f5c9d7cd962c8af725d56935ca28df2ad028
Joy Lin <vvlnote@hotmail.com>
2019-06-03 05:19:16 +0000

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -9,6 +9,32 @@ class Captain < ActiveRecord::Base
     find_captains("Sailboat")
   end
 
+  def self.talented_seafarers
+    captains_1 = find_captains("Motorboat")
+    captains = []
+    captains_1.each do |captain|
+      captain.boats.each do |b|
+        b.classifications.each do |c|
+          if c.name == 'Sailboat'
+            captains << captain
+          end
+        end
+      end
+    end
+    captains.uniq
+  end
+
+  def self.non_sailors
+    sailor_captains = find_captains("Sailboat")
+    captains = []
+    self.all.each do |c|
+      if !sailor_captains.include?(c)
+        captains << c
+      end
+    end
+    captains.uniq
+  end
+
   private
   def self.find_captains (boat_type)
     captains = []
